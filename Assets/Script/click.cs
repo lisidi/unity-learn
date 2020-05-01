7a30b5ba2bd76ec5076635f6d79702d7342f4814
李思迪 <lisidinetwork@gmail.com>
2020-05-01 16:42:54 +0800

    跳转detail场景

---
+++ Assets/Script/click.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class click : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void buttonClick()
+    {
+        Debug.Log("点击了");
+        EditorSceneManager.LoadScene("detail");
+    }
+}
