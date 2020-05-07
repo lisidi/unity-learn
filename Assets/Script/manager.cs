4c5a53ac50352801fcfc2cc4061fb645b3efab2e
李思迪 <lisidinetwork@gmail.com>
2020-05-07 17:10:39 +0800

    代码整理

--- Assets/Script/manager.cs
+++ Assets/Script/manager.cs
@@ -1,88 +1,84 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System;
-using System.Collections.Generic;
-using TMPro;
-using BestHTTP;
-using System.Text;
-using Newtonsoft.Json;
+﻿    using UnityEngine;
+    using UnityEngine.UI;
+    using System;
+    using System.Collections.Generic;
+    using TMPro;
+    using BestHTTP;
+    using System.Text;
+    using Newtonsoft.Json;
 
-public class manager : MonoBehaviour {
+    public class manager : MonoBehaviour {
 
-	void Start() {
-		Button btn = this.GetComponent<Button>();
-		btn.onClick.AddListener(delegate {
-			OnClick(btn.name);
-		});
-	}
+        void Start() {
+	        Button btn = this.GetComponent<Button>();
+	        btn.onClick.AddListener(delegate {
+		        OnClick(btn.name);
+	        });
+        }
 
-	void OnRequestFinished(HTTPRequest request, HTTPResponse response) {
- 　　    Debug.Log(response);
-        if (response.StatusCode == 200) {
-			Dictionary<string, object> values = JsonConvert.DeserializeObject<Dictionary<string, object>>(response.DataAsText);
-			var v = values["errno"];
-            if (Convert.ToInt32(v) == 0) {
-                Debug.Log("登录成功");
-            }
-            else {
-                Debug.Log("登录失败");
-            }
-		}
-    }
+        void OnRequestFinished(HTTPRequest request, HTTPResponse response) {
+ 　　         Debug.Log(response);
+            if (response.StatusCode == 200) {
+		        Dictionary<string, object> values = JsonConvert.DeserializeObject<Dictionary<string, object>>(response.DataAsText);
+		        var errno = values["errno"];
+                if (Convert.ToInt32(errno) == 0) {
+                    Debug.Log("登录成功");
+                } else {
+                    Debug.Log("登录失败");
+                }
+	        }
+        }
 
-private void OnClick(string name) {
+	    private void OnClick(string name) {
+		    switch (name) {
+			    case "UGUI":
+				    UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+				    break;
+			    case "Login":
+				    UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+				    break;
+			    case "Back":
+				    UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+				    break;
+			    case "Raw Image":
+				    break;
+			    case "Button":
+				    break;
+			    case "Input Field":
+				    break;
+			    case "Toggle":
+				    break;
+			    case "Slider":
+				    break;
+			    case "Dropdown":
+				    break;
+			    case "ImageLoad":
+				    UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+				    break;
+			    case "LoginButton":
+				    Debug.Log("LoginButton");
+				    TMP_InputField accountInputFiled = GameObject.Find("TextMeshPro - InputField - account").GetComponent<TMP_InputField>();
+				    TMP_InputField passwordInputFiled = GameObject.Find("TextMeshPro - InputField - password").GetComponent<TMP_InputField>();
+				    if (accountInputFiled.text.Length == 0 || passwordInputFiled.text.Length == 0) {
+					    Debug.Log("账号或密码为空");
+				    } else {
+					    HTTPRequest request = new HTTPRequest(new Uri("https://www.yibbuda.com/admin/auth/login"), HTTPMethods.Post, OnRequestFinished);
+					    Dictionary<string, string> requestParamsDic = new Dictionary<string, string>();
+					    requestParamsDic.Add("username", accountInputFiled.text);
+					    requestParamsDic.Add("password", passwordInputFiled.text);
+					    string requestParamsString = JsonConvert.SerializeObject(requestParamsDic);
+					    request.RawData = Encoding.UTF8.GetBytes(requestParamsString);
+					    request.AddHeader("Accept", "application/json");
+					    request.AddHeader("Content-Type", "application/json;charset=UTF-8");
+					    request.Send();
+				    }
+				    break;
+			    default:
+				    Debug.Log("点击");
+				    break;
+		    }
 
-		switch (name) {
-			case "UGUI":
-				Debug.Log("Text1");
-                UnityEngine.SceneManagement.SceneManager.LoadScene(1);
-                break;
-			case "Login":
-				Debug.Log("Text1");
-				UnityEngine.SceneManagement.SceneManager.LoadScene(2);
-				break;
-			case "Back":
-				Debug.Log("Back");
-				UnityEngine.SceneManagement.SceneManager.LoadScene(0);
-				break;
-			case "Raw Image":
-				break;
-			case "Button":
-				break;
-			case "Input Field":
-				break;
-			case "Toggle":
-				break;
-			case "Slider":
-				break;
-			case "Dropdown":
-				break;
-			case "LoginButton":
-				Debug.Log("LoginButton");
-				TMP_InputField accountInputFiled = GameObject.Find("TextMeshPro - InputField - account").GetComponent<TMP_InputField>();
-				TMP_InputField passwordInputFiled = GameObject.Find("TextMeshPro - InputField - password").GetComponent<TMP_InputField>();
-                if(accountInputFiled.text.Length == 0 || passwordInputFiled.text.Length == 0) {
-					Debug.Log("空");
-				} else {
-					
-					HTTPRequest request = new HTTPRequest(new Uri("https://www.yibbuda.com/admin/auth/login"), HTTPMethods.Post, OnRequestFinished);
-					Dictionary<string, string> requestParamsDic = new Dictionary<string, string>();
-					requestParamsDic.Add("username", accountInputFiled.text);
-					requestParamsDic.Add("password", passwordInputFiled.text);
-					string requestParamsString = JsonConvert.SerializeObject(requestParamsDic);
-					request.RawData = Encoding.UTF8.GetBytes(requestParamsString);
-					request.AddHeader("Accept", "application/json");
-					request.AddHeader("Content-Type", "application/json;charset=UTF-8");
-					request.Send();
-				}
-
-				break;
-			default:
-				Debug.Log("点击");
-				break;
-		}
-
-	}
+	    }
 
 
 }
